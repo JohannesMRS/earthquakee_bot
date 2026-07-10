27a7ba3e88bc10d5ef976699fc0bc9ffe22c3441
JohannesMRS <mariojr8304@gmail.com>
2026-07-10 11:40:26 +0700

    Feat: Integration nodejs to telegram

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-const telegramBot = require("node-telegram-bot-api");
+const {TelegramBot} = require("node-telegram-bot-api");
 
 const token = "8928162710:AAGkd7HIoX1ZFvRBlO3tPRO4s4zbbXSWG8g";
+
+
+const option = {
+    polling: true
+}
+const earthqueakeebot = new TelegramBot(token, option);
+
+const prefix = "/";
+
+const gempa = new RegExp(`^${prefix}gempa$`);
+
+earthqueakeebot.onText(gempa, async(callback)=>{
+    const BMKG_ENDPOINT = "https://data.bmkg.go.id/DataMKG/TEWS/";
+    const apiCall = await fetch(BMKG_ENDPOINT + "autogempa.json");
+    const {Infogempa: { gempa: {Tanggal, Jam, Magnitude, Kedalaman, Shakemap} }} = await apiCall.json();
+    const BMKImage = BMKG_ENDPOINT + Shakemap;
+    const hasil = `
+        Tanggal: ${Tanggal} \nJam: ${Jam} \nMagnitude: ${Magnitude} \nKedalaman: ${Kedalaman}
+    `
+
+    earthqueakeebot.sendPhoto(callback.from.id, BMKImage, {
+        caption: hasil,
+    })
+})
+
